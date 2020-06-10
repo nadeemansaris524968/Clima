4cdd097a99a3b0de54a0364e741008ec58594879
Nadeem Ansari <nadeem_ansari64@outlook.com>
2020-06-10 10:55:14 -0400

    Add code to call WeatherManager on city name entry.

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -4,21 +4,57 @@
 //
 //  Created by Angela Yu on 01/09/2019.
 //  Copyright © 2019 App Brewery. All rights reserved.
-//
+//  
 
 import UIKit
 
-class WeatherViewController: UIViewController {
-
+class WeatherViewController: UIViewController, UITextFieldDelegate, WeatherManagerDelegate {
+    
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchTextfield: UITextField!
+    
+    var weatherManager = WeatherManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        searchTextfield.delegate = self
+        weatherManager.delegate = self
     }
 
-
+    @IBAction func searchPressed(_ sender: UIButton) {
+        guard searchTextfield.text != nil else { return }
+        searchTextfield.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        guard textField.text != nil else { return false }
+        textField.endEditing(true)
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != "" {
+            return true
+        } else {
+            textField.placeholder = "Type something"
+            return false
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        guard let city = textField.text else { return }
+        weatherManager.fetchWeather(for: city)
+    }
+    
+    func didUpdateWeather(with weatherModel: WeatherModel) {
+        DispatchQueue.main.async {
+            self.temperatureLabel.text = weatherModel.temperatureString
+        }
+    }
+    
+    func didFailWith(_ error: Error) {
+        print(error)
+    }
 }
-
